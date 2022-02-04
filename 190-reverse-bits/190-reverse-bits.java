7170df826756893d201e63e454c1098767e23429
Surabhi <63421868+Surabhi16112000@users.noreply.github.com>
2022-02-04 14:04:43 +0400

    Time: 1 ms (96.47%), Space: 42.5 MB (5.09%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -6,7 +6,7 @@ public class Solution {
     int result = 0;
     for (int i = 0; i < 32; i++) {
         result = result<< 1;
-        if ((n & 1) == 1) result++;
+        if ((n & 1) == 1) result= result | 1;
         n =n>> 1;
     }
     return result;
