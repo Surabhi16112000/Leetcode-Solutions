fd1101eaeed7b53449ee85556fca948d7abfcb0a
Surabhi <63421868+Surabhi16112000@users.noreply.github.com>
2022-03-21 00:09:44 +0400

    Time: 26 ms (96.45%), Space: 52.1 MB (85.71%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,20 +1,35 @@
 class FreqStack {
-    HashMap<Integer, Integer> freq = new HashMap<>();
-    HashMap<Integer, Stack<Integer>> m = new HashMap<>();
-    int maxfreq = 0;
-
-    public void push(int x) {
+    HashMap<Integer,Stack<Integer>> hm;
+    int maxFreq=0;
+    HashMap<Integer,Integer> freq;
+    public FreqStack() {
+        hm=new HashMap<Integer,Stack<Integer>>();
+        freq=new HashMap<Integer,Integer>();
+        
+    }
+    
+    public void push(int x) 
+    {
+        
         int f = freq.getOrDefault(x, 0) + 1;
         freq.put(x, f);
-        maxfreq = Math.max(maxfreq, f);
-        if (!m.containsKey(f)) m.put(f, new Stack<Integer>());
-        m.get(f).add(x);
+        maxFreq = Math.max(maxFreq, f);
+        if (!hm.containsKey(f)) hm.put(f, new Stack<Integer>());
+        hm.get(f).add(x);
+    
+        
     }
-
-    public int pop() {
-        int x = m.get(maxfreq).pop();
-        freq.put(x, maxfreq - 1);
-        if (m.get(maxfreq).size() == 0) maxfreq--;
-        return x;
+    
+    public int pop() 
+    {
+        Stack<Integer> currList=hm.get(maxFreq);
+        int ans=currList.pop();
+        freq.put(ans,freq.get(ans)-1);
+        if(currList.isEmpty()==true)
+        {
+            maxFreq--;
+        }
+        return ans;
+        
     }
 }
