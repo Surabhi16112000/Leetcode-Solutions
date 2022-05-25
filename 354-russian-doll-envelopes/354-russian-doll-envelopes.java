652bf358f9e3abe8d018686ba2cd87ca3ba8eb54
Surabhi <63421868+Surabhi16112000@users.noreply.github.com>
2022-05-25 20:01:11 +0400

    Time: 95 ms (20.63%), Space: 77.2 MB (87.60%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length, len = 0;
+        
+        // We sort According to Width!!
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        
+        int dp[] = new int[n];
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if(index < 0)
+                index = -(index + 1);
+            dp[index] = envelope[1];
+            if(index == len)
+                len++;
+        }
+		
+        return len;
+    }   
+
+}
