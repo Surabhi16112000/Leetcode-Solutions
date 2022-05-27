17b22a9730fed965965a12ce74a79e04f3daa0ce
Surabhi <63421868+Surabhi16112000@users.noreply.github.com>
2022-05-27 23:27:06 +0400

    Time: 19 ms (27.98%), Space: 44.4 MB (21.33%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,21 +1,42 @@
 class Solution {
-    public String multiply(String num1, String num2) {
-    int m = num1.length(), n = num2.length();
-    int[] pos = new int[m + n];
-   
-    for(int i = m - 1; i >= 0; i--) {
-        for(int j = n - 1; j >= 0; j--) {
-            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
-            int p1 = i + j, p2 = i + j + 1;
-            int sum = mul + pos[p2];
+    public String multiply(String num1, String num2) 
+    {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
+        int pf=0;
+        
+        for(int i = m - 1; i >= 0; i--) 
+        {
+            int ival=num1.charAt(i)-'0';
+            int ind=pos.length-1-pf;
+            pf++;
+            int carry=0;
+            int j=n-1;
+            while(j>=0 || carry!=0)
+            {
+                int jval=0;
+                if(j>=0)
+                {
+                    jval=num2.charAt(j)-'0';
+                }
+                int prod=ival * jval + carry + pos[ind];
+                pos[ind]=prod%10;
+                carry=prod/10;
+                ind--;
+                j--;
+            }
+        }  
 
-            pos[p1] += sum / 10;
-            pos[p2] = (sum) % 10;
-        }
-    }  
-    
-    StringBuilder sb = new StringBuilder();
-    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
-    return sb.length() == 0 ? "0" : sb.toString();
-}
+        String ans="";
+        boolean f=false;
+        for(int p : pos) 
+            if(f==false && p==0) continue;
+            else 
+            {
+                f=true;
+                ans+=""+p;
+            }
+        return ans;
+    }
 }
