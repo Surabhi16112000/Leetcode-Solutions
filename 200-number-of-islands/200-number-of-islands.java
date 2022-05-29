9ad899d23007db96f0d3841fc7be4a37c75ee60a
Surabhi <63421868+Surabhi16112000@users.noreply.github.com>
2022-05-30 00:51:46 +0400

    Time: 9 ms (22.54%), Space: 57.7 MB (33.43%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,42 @@
+class Solution {
+    static boolean[][] vis;
+    int[] dir={1,0,-1,0,1};
+    public boolean isValid(int i, int j, int r, int c)
+    {
+        return i>=0 && i<r && j>=0 && j<c;
+    }
+    public void dfs(int i, int j, int m ,int n, char[][] a)
+    {
+        if(i>=m || j>=n || i<0 || j<0) return;
+        vis[i][j]=true;
+        for(int k=0;k<4;k++)
+        {
+            int new_i=i+dir[k];
+            int new_j=j+dir[k+1];
+            if(a[i][j]=='1' && isValid(new_i, new_j, m,n) && vis[new_i][new_j]==false)
+                dfs(i+dir[k], j+dir[k+1], m, n, a);
+            
+        }
+        return;
+    }
+    public int numIslands(char[][] a) 
+    {
+        int m=a.length;
+        int n=a[0].length;
+        vis=new boolean[m][n];
+        int ans=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(!vis[i][j] && a[i][j]=='1')
+                {
+                    ans++;
+                    dfs(i,j,m,n,a);
+                    
+                }
+            }
+        }
+        return ans;
+    }
+}
